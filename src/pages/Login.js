d4500fd791b6750b447edbad41cd343c9ac2cd52
avner098 <avnerbenshlomo@gmail.com>
2023-06-10 23:14:42 +0300

    update frontend

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -16,7 +16,7 @@ const Login = ()=>{
         setLoading(true)
         setError(null)
 
-        const response = await fetch('/api/user/Login',{
+        const response = await fetch('https://to-do-list-backend-theta.vercel.app/api/user/Login',{
             method: 'POST',
             headers: {'Content-Type' : 'application/json'},
             body: JSON.stringify({email,password})
