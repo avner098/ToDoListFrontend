fa2b298e13fe71284eea5f19b0e4edbaaef6084b
avner098 <avnerbenshlomo@gmail.com>
2023-06-10 22:23:01 +0300

    update frontend

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,13 +10,13 @@ const Login = ()=>{
 
     const context = useContext(AuthContext)
 
-
+    
 
     const login = async(email,password) =>{
         setLoading(true)
         setError(null)
 
-        const response = await fetch('/api/user/Login',{
+        const response = await fetch('https://to-do-list-backend-theta.vercel.app/api/user/Login',{
             method: 'POST',
             headers: {'Content-Type' : 'application/json'},
             body: JSON.stringify({email,password})
